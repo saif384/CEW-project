7614f3a670b131d2a8fefa0f18d8c0599fda73e9
saif384 <148412425+saif384@users.noreply.github.com>
2024-01-13 17:15:53 +0000

    this code is used to call the mail code in main

---
+++ email_main.c
@@ -0,0 +1,27 @@
+#include "email_sender.h"
+#include <stdio.h>
+
+
+int main() {
+	 FILE *finalize = fopen("work.txt", "wb");
+    	if (!finalize) {
+        	fprintf(stderr, "Failed to open file for writing.\n");
+        	return 1;
+    }
+    fprintf(finalize,"hello");
+    fclose(finalize);
+    
+    const char *to = "hafizabdulbaseer565@gmail.com";
+    const char *file_path = "work.txt";
+    
+
+    int result = send_email_with_attachment(to, file_path);
+
+    if (result == 0) {
+        printf("Email sent successfully.\n");
+    } else {
+        printf("Failed to send email.\n");
+    }
+
+    return 0;
+}
